261803fd935eaa460df79c164035b6f81bcb3169
dkelly255 <dkelly25@hotmail.co.uk>
2022-01-18 19:05:27 +0000

    Start adding Authentication functionality

--- fantasyfootballcentre/urls.py
+++ fantasyfootballcentre/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("website.urls"), name="website-urls"),
     path('summernote/', include('django_summernote.urls')),
+    path("accounts/", include("allauth.urls")),
 ]
