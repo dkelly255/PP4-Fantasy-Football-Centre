49b48bdbebc5cf2d7b50118e6a1ca46227f7af60
dkelly255 <dkelly25@hotmail.co.uk>
2022-02-07 16:03:16 +0000

    Toggle debug flag approach to troubleshoot deployment CSS format issue

--- fantasyfootballcentre/settings.py
+++ fantasyfootballcentre/settings.py
@@ -30,7 +30,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+# Note:Had to disable env variable approach to troubleshoot heroku formatting
+# DEBUG = os.environ.get('DEBUG')
+DEBUG = False
 
 ALLOWED_HOSTS = ['fantasyfootballcentre.herokuapp.com', 'localhost']
 
