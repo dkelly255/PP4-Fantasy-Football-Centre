d9bc4242b73a9aa7852d88ace8da92c7d44e1c6c
dkelly255 <dkelly25@hotmail.co.uk>
2022-02-14 11:35:08 +0000

    Change debug flag to development to resolve admin panel bug

--- fantasyfootballcentre/settings.py
+++ fantasyfootballcentre/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # Note:Had to disable env variable approach to troubleshoot heroku formatting
 # DEBUG = os.environ.get('DEBUG')
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['fantasyfootballcentre.herokuapp.com', 'localhost']
 
@@ -157,7 +157,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
